2c8c1264f4ca80906bf80719b72edf64ff84c50c
hsjoihs <hirotaka_satou@dwango.co.jp>
2025-07-19 18:16:03 +0900

    Implement

--- FlowregexLean/BitVecDistrLattice.lean
+++ FlowregexLean/BitVecDistrLattice.lean
@@ -30,3 +30,68 @@ def BitVec.toFinsetFin {m : Nat} (bv : BitVec m) : Finset (Fin m) :=
   let zipped : List (Bool × Fin m) := list.zipIdxFin length_proof 0
   let indices : List (Fin m) := zipped |>.filter (fun x => x.1) |>.map Prod.snd
   Finset.filter (fun i => i ∈ indices) Finset.univ
+
+
+def rangeloop_length {n : Nat} {list : List Nat}
+  : (List.range.loop n list).length = n + list.length := by
+  induction n generalizing list with
+  | zero => simp [List.range.loop]
+  | succ n ih =>
+    unfold List.range.loop
+    have ih2 := @ih (n :: list)
+    simp [ih]
+    ac_rfl
+
+def range_length {n : Nat} : (List.range n).length = n := by
+  unfold List.range
+  have h := @rangeloop_length n []
+  simp [h]
+
+def map_length {α β : Type} (f : α -> β) (l : List α) :
+  (l.map f).length = l.length := by
+  induction l with
+  | nil => rfl
+  | cons a l ih => simp [ih]
+
+set_option pp.proofs true
+
+def zipIdxFin_length {α} {l : List α} {n : Nat} {m : Nat} (h_len : m = l.length)  :
+  (l.zipIdxFin h_len n).length = m := by
+  induction l generalizing m n with
+  | nil =>
+    simp at h_len
+    simp [List.zipIdxFin, h_len]
+  | cons a l ih =>
+    unfold List.zipIdxFin
+    simp
+    simp at h_len
+    have ih2 := @ih (n + 1) l.length (by rfl)
+    simp [ih2]
+    exact h_len.symm
+
+def BitVec.fromFinsetFin {m : Nat} (s : Finset (Fin m)) : BitVec m :=
+  -- We need a list of length `m` whose values are indices that are in `Fin m`.
+  -- We first create a list of length `m` by using `List.range m`,
+  let h_range_len : m = (List.range m).length := (@range_length m).symm
+
+  -- And then we zip it with `Fin m` indices.
+  let range_range : List (Nat × Fin m) := (List.range m).zipIdxFin h_range_len 0
+
+  -- Of course, the length is still `m`.
+  have h_range_range_len : m = range_range.length := by
+    have len1 : ((List.range m).zipIdxFin h_range_len 0).length = m := zipIdxFin_length h_range_len
+    symm
+    unfold range_range
+    exact len1
+
+  -- Now we transcribe the membership of `s` into a list of `Bool` values.
+  let little_endian : List Bool := range_range.map (fun (_i, i) => i ∈ s)
+
+  -- Of course, the length is still `m`.
+  let length_proof : m = little_endian.length := by
+    have len1 := map_length (fun (_i, i) => i ∈ s) range_range
+    unfold little_endian
+    simp [h_range_range_len]
+
+  let ans : BitVec (little_endian.length) := BitVec.ofBoolListLE little_endian
+  BitVec.cast length_proof.symm ans
